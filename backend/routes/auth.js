c398589395c78aae85b5cf6dc6e6ebec9c9d9026
NM <masihnishit@gmail.com>
2022-09-10 12:20:56 +0530

    #45 video

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,13 @@
 const express=require('express');
+const User = require('../models/User');
 const router=express.Router();
 
-router.get('/', (req,res)=>{
-    obj={
-        name:'tom',
-        age:20
-    }
-    res.json(obj);
+// create a user using: POST "/api/auth/". doesn't require Auth
+router.post('/', (req,res)=>{
+    console.log(req.body);
+    const user=User(req.body);
+    user.save();
+    res.send(req.body);
 })
 
 module.exports=router;
