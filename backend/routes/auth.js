99099f14d40743ce8510f34defa488e5409a8056
NM <masihnishit@gmail.com>
2022-09-12 12:09:20 +0530

    #46 video

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,30 @@
 const express=require('express');
 const User = require('../models/User');
 const router=express.Router();
+const { body, validationResult } = require('express-validator');
 
 // create a user using: POST "/api/auth/". doesn't require Auth
-router.post('/', (req,res)=>{
-    console.log(req.body);
-    const user=User(req.body);
-    user.save();
-    res.send(req.body);
+router.post('/', 
+[
+    body('name','enter valid name').isLength({ min: 3 }),
+    body('email','enter valid email').isEmail(),
+    body('password','password must be atleast 5 character').isLength({ min: 5 })
+], 
+(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    }).then(user => res.json(user))
+    .catch(err=>{
+      console.log(err),
+      res.json({error:'please enter a unique email',message: err.message})
+    });
 })
 
 module.exports=router;
