248481db1a0bde63bf45e9caf9867e23797fcefc
NM <masihnishit@gmail.com>
2022-09-12 19:00:14 +0530

    #50 video

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -46,8 +46,44 @@ async (req,res)=>{
       res.json(authToken)
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("some error occured");
+      res.status(500).send("internal server error");
     }
+})
+
+// authenticate a user using: POST "/api/auth/login". no login required
+router.post('/login', 
+[
+    body('email','enter valid email').isEmail(),
+    body('password','password cannot be blank').exists()
+], 
+async (req,res)=>{
+  // if there are errors, return bad request and the errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const {email,password}=req.body;
+  try {
+    let user=await User.findOne({email});
+    if (!user) {
+      return res.status(400).json({error:"please try to login with correct credentails"});
+    }
+    const passwordCompare=await bcrypt.compare(password,user.password);
+    if (!passwordCompare) {
+      return res.status(400).json({error:"please try to login with correct credentails"});
+    }
+    const data={
+      user:{
+        id: user.id
+      }
+    }
+    const authToken=jwt.sign(data,JWT_SECRET);
+    res.json(authToken)
+  }  catch (error) {
+    console.error(error.message);
+    res.status(500).send("internal server error");
+  }
 })
 
 module.exports=router;
