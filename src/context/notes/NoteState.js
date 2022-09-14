99d0173411a71613b8e08d82b2d052d281ccddcf
NM <masihnishit@gmail.com>
2022-09-14 15:37:47 +0530

    #59 video

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,22 +1,8 @@
-import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState= (props)=>{
-    const state1={
-        "name":"tom",
-        "age":"25"
-    }
-    const [state,setState]=useState(state1);
-    const update= ()=>{
-        setTimeout(() => {
-            setState({
-                "name":"jerry",
-                "age":"50"
-            })
-        }, 1000);
-    }
     return(
-        <NoteContext.Provider value={{state, update}}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     )
